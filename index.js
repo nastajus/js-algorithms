a1e4fdc2482a584ad7f29219bd8b50ff6005f1c1
Ian Nastajus <ian.nastajus@gmail.com>
2018-05-03 01:37:52 -0400

    BubbleSort, along with the concept of a namespace.

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ var js_algorithms_namespace = {
 
 	main : function () {
 
+		this.bubbleSort( [2, 1] );
+		this.bubbleSort( [3,2,1] );
+		this.bubbleSort( [4,3,2,1] );
+
 	},
 
-	bubbleSort : function () {
+	bubbleSort : function ( array ) {
+		if (!Array.isArray(array)) return array;
 
+		for (var i = 0; i < array.length; i++)
+		{
+			for (var j = 0; j < array.length; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					//swap
+					var tempValue = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = tempValue;
+				}
+			}
+		}
+		console.log ( array );
+
+		return array;
 	},
 
 	selectionSort : function () {
